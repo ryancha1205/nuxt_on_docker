537e84a2f75ba95b9c15bb1f2aa31ea2326725f5
Jeongmin Cha <ryancha1205@gmail.com>
2018-10-13 12:41:38 +0900

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="post-list">
+    <PostPreview id="1" thumbnail="https://www.visioncritical.com/wp-content/uploads/2015/12/tech-companies-resources-950x700.jpg" title="hello there!" previewText="This is my first post!" />
+    <PostPreview id="2" thumbnail="https://www.visioncritical.com/wp-content/uploads/2015/12/tech-companies-resources-950x700.jpg" title="good" previewText="good job" />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+<style>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
